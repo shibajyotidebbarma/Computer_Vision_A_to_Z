9a6528d01324277ac42e2a312b7f708244d5a16b
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2018-07-26 16:21:16 +0530

    Face Recognition Project added

---
+++ Module 1 - Face Recognition/face_recognition_nocomment.py
@@ -0,0 +1,14 @@
+# Face Recognition
+
+#Importing the libraries
+import cv2
+
+#Loading the cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#Defining a function that will do the detections
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (50,205,50), 2)
